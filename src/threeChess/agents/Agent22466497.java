e62761eb553b5b18e373248da6a3c8405167efe1
ForsakenIdol <lachlandwhang@gmail.com>
2020-09-24 15:40:43 +0800

    Lachlan's agent skeleton created with toString method completed

---
+++ src/threeChess/agents/Agent22466497.java
@@ -0,0 +1,30 @@
+package threeChess.agents;
+
+import threeChess.Agent;
+import threeChess.Board;
+import threeChess.Position;
+
+
+/**
+ * A Q-Learning agent for ThreeChess.
+ * To Do:
+ * 
+ * 
+ * Current Issues:
+ * 
+ * 
+ */
+public class Agent22466497 extends Agent {
+
+  private final String name = "Agent22466497";
+
+  public Position[] playMove(Board board) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public String toString() {return name;}
+
+  public void finalBoard(Board finalBoard) {}
+  
+}
